eab4abebfaef836ef41cbcaca10b35156cc1b563
jumagu <jmgg.gutierrez@gmail.com>
2024-04-14 21:46:43 -0500

    TopMenu Improved

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -8,6 +8,7 @@ import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 
 import { teslaFont } from "@/config/fonts.config";
 import { useCartStore, useUiStore } from "@/store";
+import clsx from "clsx";
 
 interface Props {
   isOnAuth?: boolean;
@@ -25,23 +26,36 @@ export const TopMenu = ({ isOnAuth = false }: Props) => {
 
   return (
     <header className="flex h-14 justify-between items-center bg-white w-full sticky top-0 z-30">
-      <h1 className="flex items-center pl-[2px]">
+      <h1
+        className={clsx("flex items-center", {
+          "pl-[2px]": !isOnAuth,
+          "pl-[10px] min-[600px]:pl-[22px] mlg:pl-[34px]": isOnAuth,
+        })}
+      >
         <span
-          className={`${teslaFont.className} antialiased text-base px-4 tracking-[8px] select-none mt-1`}
+          className={`${teslaFont.className} antialiased text-base pl-4 sm:pr-4 tracking-[8px] select-none mt-1`}
         >
           Teslo
         </span>
 
-        <span className="font-medium text-[13px] pl-[6px] pr-[15px]">|</span>
+        {isOnAuth ? (
+          <></>
+        ) : (
+          <>
+            <span className="font-medium text-[12px] pl-[6px] pr-[6px] sm:pr-[15px]">
+              |
+            </span>
 
-        <Link
-          className="px-2 py-1 hover:bg-gray-100 transition-all duration-[.33s]"
-          href="/products"
-        >
-          <span className="tracking-[1.4px] font-medium text-[13.5px]">
-            Shop
-          </span>
-        </Link>
+            <Link
+              className="px-2 py-1 hover:bg-gray-100 transition-all duration-[.33s]"
+              href="/products"
+            >
+              <span className="tracking-[1.4px] font-medium text-[13px]">
+                Shop
+              </span>
+            </Link>
+          </>
+        )}
       </h1>
 
       {isOnAuth ? (
@@ -53,7 +67,7 @@ export const TopMenu = ({ isOnAuth = false }: Props) => {
               <li role="none">
                 <div className="py-1 px-2 hover:bg-gray-100 transition-all duration-[.33s]">
                   <Link
-                    className="mx-2 text-[13.5px] text-gray-600 font-medium tracking-[1.8px] hover:text-black transition-all duration-[.33s]"
+                    className="mx-2 text-[13px] text-gray-600 font-medium tracking-[1.8px] hover:text-black transition-all duration-[.33s]"
                     href="/gender/men"
                   >
                     Men
@@ -64,7 +78,7 @@ export const TopMenu = ({ isOnAuth = false }: Props) => {
               <li role="none">
                 <div className="py-1 px-2 hover:bg-gray-100 transition-all duration-[.33s]">
                   <Link
-                    className="mx-2 text-[13.5px] text-gray-600 font-medium tracking-[1.8px] hover:text-black transition-all duration-[.33s]"
+                    className="mx-2 text-[13px] text-gray-600 font-medium tracking-[1.8px] hover:text-black transition-all duration-[.33s]"
                     href="/gender/women"
                   >
                     Women
@@ -75,7 +89,7 @@ export const TopMenu = ({ isOnAuth = false }: Props) => {
               <li role="none">
                 <div className="py-1 px-2 hover:bg-gray-100 transition-all duration-[.33s]">
                   <Link
-                    className="mx-2 text-[13.5px] text-gray-600 font-medium tracking-[1.8px] hover:text-black transition-all duration-[.33s]"
+                    className="mx-2 text-[13px] text-gray-600 font-medium tracking-[1.8px] hover:text-black transition-all duration-[.33s]"
                     href="/gender/kids"
                   >
                     Kids
@@ -86,10 +100,8 @@ export const TopMenu = ({ isOnAuth = false }: Props) => {
           </nav>
 
           <ol className="flex items-center pr-4">
-
-            
             <div className="mr-2 hidden mlg:block">
-              <IoSearchOutline size={20} />
+              <IoSearchOutline size={22} />
             </div>
 
             <div className="ml-1">
@@ -114,12 +126,12 @@ export const TopMenu = ({ isOnAuth = false }: Props) => {
               </Link>
             </div>
 
-            <div className="ml-2">
+            <div className="ml-[14px] mlg:ml-2">
               <button
-                className="px-2 py-1 hover:bg-gray-100 transition-all duration-[.33s] uppercase"
+                className="sm:px-2 py-1 hover:bg-gray-100 transition-all duration-[.33s] uppercase"
                 onClick={openSideBarMenu}
               >
-                <span className="mx-2 text-[13.5px] text-gray-600 font-medium tracking-[1.8px] leading-[26px] hover:text-black transition-all duration-[.33s]">
+                <span className="mx-2 text-[13px] text-gray-600 font-medium tracking-[1.8px] leading-[26px] hover:text-black transition-all duration-[.33s]">
                   Menu
                 </span>
               </button>
