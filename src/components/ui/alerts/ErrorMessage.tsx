0dbe6875152270d64742c406296d6ab6ea675644
jumagu <jmgg.gutierrez@gmail.com>
2024-08-16 01:44:26 -0500

    Renamed "error-message" folder to "alerts"

--- src/components/ui/alerts/ErrorMessage.tsx
+++ src/components/ui/alerts/ErrorMessage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { RiErrorWarningFill } from "react-icons/ri";
 
 interface Props {
@@ -8,9 +10,10 @@ interface Props {
 export const ErrorMessage = ({ message, className }: Props) => {
   return (
     <div
-      className={`fade-in flex gap-2 bg-gray-100 min-h-[46px] py-[10px] px-4 select-none ${className}`}
+      role="alert"
       aria-live="polite"
       aria-atomic="true"
+      className={`fade-in flex gap-2 bg-gray-100 min-h-[46px] py-[10px] px-4 select-none ${className}`}
     >
       <RiErrorWarningFill className="min-h-6 min-w-6 text-red-500" />
       <p className="font-medium text-sm text-gray-700">{message}</p>
