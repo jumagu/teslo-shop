7beff27dabac64c784e6a57c878651939009cb5b
jumagu <jmgg.gutierrez@gmail.com>
2024-08-18 01:34:36 -0500

    * Add generic type to SelectOption type * Add className prop to BaseFormField interface

--- src/interfaces/form.interface.ts
+++ src/interfaces/form.interface.ts
@@ -2,15 +2,16 @@ import type { RegisterOptions } from "react-hook-form";
 
 export type FormFieldErrorVariant = "md" | "sm";
 export type FormFieldVariant = "input" | "select" | "textarea";
-export type SelectOption = {
+export type SelectOption<T = string> = {
   name: string;
-  value: string;
+  value: T;
 };
 
 export interface BaseFormField {
   name: string;
   label: string;
   disabled?: boolean;
+  className?: string;
   validations?: RegisterOptions<any>;
   errorVariant?: FormFieldErrorVariant;
 }
