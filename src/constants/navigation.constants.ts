45e0c67b00d818f543ad8e482a8730e511468294
jumagu <jmgg.gutierrez@gmail.com>
2024-08-19 12:34:26 -0500

    * Add new directory for storing constants. * Add constants related to navigation options.

---
+++ src/constants/navigation.constants.ts
@@ -0,0 +1,16 @@
+export const GENDERS = [
+  { path: "/gender/men", name: "Men" },
+  { path: "/gender/women", name: "Women" },
+  { path: "/gender/kids", name: "Kids" },
+];
+
+export const AUTHENTICATED_OPTIONS = [
+  { path: "/profile", name: "Profile" },
+  { path: "/orders", name: "Orders" },
+];
+
+export const ADMIN_OPTIONS = [
+  { path: "/admin/products", name: "Products" },
+  { path: "/admin/orders", name: "Orders" },
+  { path: "/admin/users", name: "Users" },
+];
